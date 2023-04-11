d2675aafbf6f9632747615999aba37f1eedf0028
Pedro Nascente <nascente3d@hotmail.com>
2023-04-11 13:10:37 -0300

    modificado  include/footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,15 @@
 <!-- FOOTER -->
 <footer class="footer">
 	<a href="javascript:" id="return-to-top"><i class="fa fa-angle-up fa-3x" aria-hidden="true"></i></a>
-	<div class="footer-top">				
-		<div class="container">					
-			<div class="row">						
-				<div class="col-xs-12 col-md-4">							
+	<div class="footer-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-4">
 					<p><img alt="logo-footer" src="public/images/logo-2.png" class="logo"></p>
 					<p>Promovendo soluções em segurança para uma vida melhor.</p>
-					<p><a target="_self" data-toggle="modal" data-target="#myModal" class="btn btn-danger">Peça seu orçamento</a></p>				
-				</div>						
-				<div class="col-xs-12 col-md-2">							
+					<p><a target="_self" data-toggle="modal" data-target="#myModal" class="btn btn-danger">Peça seu orçamento</a></p>
+				</div>
+				<div class="col-xs-12 col-md-2">
 					<div class="widget_nav_menu">
 						<h6 class="footer-top__headings">NAVEGAÇÃO</h6>
 						<ul>
@@ -20,20 +20,20 @@
 							<li><a href="/contato">Contato</a></li>
 							<li><a href="/politica-de-privacidade">Politica de Privacidade</a></li>
 						</ul>
-					</div>							
-				</div>						
-				<div class="col-xs-12 col-md-2">							
-					<div class="widget_nav_menu">								
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-2">
+					<div class="widget_nav_menu">
 						<h6 class="footer-top__headings">SOLUÇÕES</h6>
 						<ul>
 							<li><a href="https://www.volpatorastreamento.com.br">Rastreamento Veicular</a></li>
 							<li><a href="/seguranca-eletronica">Segurança Eletrônica</a></li>
 							<li><a href="https://www.portariavolpato.com.br">Serviços de Portaria</a></li>
-							<li><a href="/projetos-integrados">Projetos Integrados</a></li>									
-						</ul>								
-					</div>							
-				</div>						
-				<div class="col-xs-12 col-md-4">							
+							<li><a href="/projetos-integrados">Projetos Integrados</a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-xs-12 col-md-4">
 					<h6 class="footer-top__headings">CONTATO</h6>
 					<p>
 						<strong>Matriz</strong><br>
@@ -43,7 +43,7 @@
 					<p>
 						<strong>Fone:</strong> 3004-5554<br>
 						<strong>E-mail:</strong> volpato@grupovolpato.com
-					</p>							
+					</p>
 					<p>
 						<a target="_blank" href="https://www.facebook.com/Volpato.se/" class="icon-container icon-container--square">
 							<span class="fa fa-facebook"></span>
@@ -59,31 +59,31 @@
 							<span class="fa fa-youtube-play"></span>
 						</a>
 					</p>
-				</div>					
-			</div>					
-		</div>				
-	</div>			
-	<div class="footer-bottom">				
-		<div class="container">					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="footer-bottom">
+		<div class="container">
 			<div class="footer-bottom__left">
 				(C) 2017 Todos os direitos reservados - Proibida a reprodução total ou parcial sem autorização prévia do autor.
-			</div>														
-		</div>				
-	</div>			
-</footer>		
+			</div>
+		</div>
+	</div>
+</footer>
 </div><!-- /.boxed-container -->
 
 <script src="public/js/jquery3.js" type="text/javascript"></script>
 <script src="public/js/modernizr.js" type="text/javascript"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
-<script src="public/owlcarousel/owl.carousel.js"></script>	
+<script src="public/owlcarousel/owl.carousel.js"></script>
 <script src="public/js/bootstrap3/bootstrap.min.js" type="text/javascript"></script>
-<script src="public/js/to-top.js"></script>		
+<script src="public/js/to-top.js"></script>
 <script src="public/js/valida-telefone.js"></script>
 <script type="text/javascript" src="public/js/jquery.mask.js"></script>
 
 <script type="text/javascript">
-	$(document).ready(function(){
+	$(document).ready(function() {
 		$('.phone_with_ddd').mask('(00) 00000-0000');
 		$('.ddd').mask('00');
 	});
@@ -93,26 +93,27 @@
 	$(document).ready(function() {
 		var owl = $('.owl-carousel');
 		owl.owlCarousel({
-			loop:true,
-			margin:0,
-			navSpeed:500,
-			nav:true,
-			items:1,
-			autoplay:true,
-			autoplayTimeout:3000,
-			autoplayHoverPause:true,
+			loop: true,
+			margin: 0,
+			navSpeed: 500,
+			nav: true,
+			items: 1,
+			autoplay: true,
+			autoplayTimeout: 3000,
+			autoplayHoverPause: true,
 		});
 	})
 </script>
 
 <script>
 	// Este evendo é acionado após o carregamento da página
-	$(window).on("load", function (e) {
+	$(window).on("load", function(e) {
 
-	//Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
-	jQuery("#loader").fadeOut("slow");
-});
+		//Após a leitura da pagina o evento fadeOut do loader é acionado, esta com delay para ser perceptivo em ambiente fora do servidor.
+		jQuery("#loader").fadeOut("slow");
+	});
 </script>
 
 </body>
+
 </html>
